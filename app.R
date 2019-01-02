a9c766eba0539eb8b2853bbdaf8802de94b2b437
Buzzee <benjamin.buzzee@dfg.alaska.local>
2019-01-02 14:10:46 -0900

    init2

--- app.R
+++ app.R
@@ -12,56 +12,58 @@ library(shiny)
 library(ggplot2)
 library(tidyverse)
 library(lubridate)
-# api.key <- "59f051c54cc49a42ef1f3ba3426792b8"
-# fred <- FredR(api.key)
+library(plotly)
+
+api.key <- "59f051c54cc49a42ef1f3ba3426792b8"
+fred <- FredR(api.key)
 
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
+
    # Application title
   tabsetPanel(
     tabPanel("Home",
-    
+
    titlePanel("MacroView"),
-   
-   # Sidebar with a slider input for number of bins 
+
+   # Sidebar with a slider input for number of bins
    helpText("Insert text about app")
-      
+
    ),
    tabPanel("Fed Data",
-            "Place fed data here.",
-            plotOutput("FFPlot"))
+            plotlyOutput("FFPlot"))
 )
 
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-  
-  
-  
+
+
+
+
   #=== FEDERAL FUNDS RATE PLOT ===#
-  
+
   FF <- fred$series.observations("DFF")
-  
-   output$FFPlot <- renderPlot({
+
+   output$FFPlot <- renderPlotly({
+
      FF <- fred$series.observations("DFF")
-     
-     
-     dt <-  FF %>% select(date, value) %>% as_tibble() %>% 
+
+     dt <-  FF %>% select(date, value) %>% as_tibble() %>%
        mutate(Date = as.Date(date), Rate = as.numeric(value)/100) %>%
        group_by(Date = floor_date(Date, unit = "month")) %>% summarize(Rate = mean(Rate))
-     
-     
-     dt %>% ggplot(aes(x = Date, y = Rate)) + geom_smooth(span = .25, se = FALSE, size = 1.5) + geom_line() + ggtitle("Federal Funds Rate")
-     
+
+
+     ff_gg <- dt %>% ggplot(aes(x = Date, y = Rate)) + geom_smooth(span = .25, se = FALSE, size = 1.5) + geom_line() + ggtitle("Federal Funds Rate")
+
+     return(ggplotly(ff_gg))
    })
-   
-   
+
+
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
 

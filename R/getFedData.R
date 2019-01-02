38fa504d9cf4a52f0c58f4280d7066ce9900087c
Buzzee <benjamin.buzzee@dfg.alaska.local>
2019-01-02 14:17:03 -0900

    init

---
+++ R/getFedData.R
@@ -0,0 +1,26 @@
+
+
+#' Title
+#' Fetches and returns clean data from the Federal Reserces API using the FedR package
+#' @param tag
+#'
+#' @return dt
+#' @export
+#'
+#' @examples
+#' fed_functs <- getFedData("DFF")
+getFedData <- function(tag = "DFF"){
+
+
+  if (tag == "DFF"){
+
+  FF <- fred$series.observations("tag")
+
+  dt <-  FF %>% select(date, value) %>% as_tibble() %>%
+      mutate(Date = as.Date(date), Rate = as.numeric(value)/100) %>%
+      group_by(Date = floor_date(Date, unit = "month")) %>% summarize(Rate = mean(Rate))
+
+
+  return(dt)
+  }
+
